155ce870906c49e76297204315e5910c5a3e50c7
HoasmHasanRamadan <hosam.hasan.ramadan@gmail.com>
2023-03-25 22:33:07 +0200

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,11 +1,48 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:convert';
 
-import 'package:flutter/services.dart';
 import 'package:blue/blue.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+final connectedDevicesProvider = FutureProvider((ref) async {
+  final invalidationTimer = Timer(const Duration(seconds: 1), () {
+    ref.invalidateSelf();
+  });
+  ref.onDispose(invalidationTimer.cancel);
+  return await Blue().getConnectedDevices();
+});
+
+final pairedDevicesProvider = FutureProvider((ref) async {
+  final invalidationTimer = Timer(const Duration(seconds: 1), () {
+    ref.invalidateSelf();
+  });
+  ref.onDispose(invalidationTimer.cancel);
+  return await Blue().getPairedDevices();
+});
+
+final flutterBlueConnectedDeviceProvider = FutureProvider((ref) async {
+  final invalidationTimer = Timer(const Duration(seconds: 1), () {
+    ref.invalidateSelf();
+  });
+  ref.onDispose(invalidationTimer.cancel);
+  return await FlutterBluePlus.instance.connectedDevices;
+});
+
+final flutterBluePairedDeviceProvider = FutureProvider((ref) async {
+  final invalidationTimer = Timer(const Duration(seconds: 1), () {
+    ref.invalidateSelf();
+  });
+  ref.onDispose(invalidationTimer.cancel);
+  return await FlutterBluePlus.instance.bondedDevices;
+});
+
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatefulWidget {
@@ -16,35 +53,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _bluePlugin = Blue();
+
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _bluePlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+    
   }
 
   @override
@@ -55,9 +69,117 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: Column(
+            children: [
+              Expanded(
+                child: Consumer(
+                  builder: (context, ref, child) {
+                    final connectedDevices =
+                        ref.watch(pairedDevicesProvider).valueOrNull ?? [];
+                    if (connectedDevices.isEmpty) return Text('Not Ready');
+                    return ListView(
+                      shrinkWrap: true,
+                      children: [
+                        Text('Paired'),
+                        ...connectedDevices.map(
+                          (e) => ListTile(
+                            title: Text(e.name),
+                            subtitle: Text(e.address),
+                            leading: Text(e.connected.toString()),
+                            trailing: Text(e.level.toString()),
+                          ),
+                        )
+                      ],
+                    );
+                  },
+                ),
+              ),
+              Expanded(
+                child: Consumer(
+                  builder: (context, ref, child) {
+                    final connectedDevices =
+                        ref.watch(connectedDevicesProvider).valueOrNull ?? [];
+                    if (connectedDevices.isEmpty) return Text('Not Ready');
+                    return ListView(
+                      shrinkWrap: true,
+                      children: [
+                        Text('Connected'),
+                        ...connectedDevices.map(
+                          (e) => ListTile(
+                            title: Text(e.name),
+                            subtitle: Text(e.address),
+                            leading: Text(e.connected.toString()),
+                            trailing: Text(e.level.toString()),
+                          ),
+                        )
+                      ],
+                    );
+                  },
+                ),
+              ),
+              Expanded(
+                child: Consumer(
+                  builder: (context, ref, child) {
+                    final connectedDevices = ref
+                            .watch(flutterBluePairedDeviceProvider)
+                            .valueOrNull ??
+                        [];
+                    if (connectedDevices.isEmpty) return Text('Not Ready');
+                    return ListView(
+                      shrinkWrap: true,
+                      children: [
+                        Text('Flutter Blue Paired'),
+                        ...connectedDevices.map(
+                          (e) => ListTile(
+                            title: Text(e.name),
+                            subtitle: Text(e.id.id.toString()),
+                          ),
+                        )
+                      ],
+                    );
+                  },
+                ),
+              ),
+              Expanded(
+                child: Consumer(
+                  builder: (context, ref, child) {
+                    final connectedDevices = ref
+                            .watch(flutterBlueConnectedDeviceProvider)
+                            .valueOrNull ??
+                        [];
+                    if (connectedDevices.isEmpty) return Text('Not Ready');
+                    return ListView(
+                      shrinkWrap: true,
+                      children: [
+                        Text('Flutter blue Connected'),
+                        ...connectedDevices.map(
+                          (e) => ListTile(
+                            title: Text(e.name),
+                            subtitle: Text(e.id.id),
+                          ),
+                        )
+                      ],
+                    );
+                  },
+                ),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            [
+              Permission.bluetooth,
+              Permission.bluetoothAdvertise,
+              Permission.bluetoothConnect,
+              Permission.bluetoothScan,
+            ].request();
+          },
+          child: Text('Ask'),
         ),
       ),
     );
   }
+
+ 
 }
